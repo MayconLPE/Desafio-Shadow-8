803319dcd5f6fd6e7606798340fc782ff4f22cbe
MayconL27 <mayconleandro2008@gmail.com>
2023-03-24 14:37:45 -0300

    Exception CPF ou CNPJ já cadastrados

--- Banco/src/main/java/com/banco/Main/useCases/exceptionHandler/Exceptions.java
+++ Banco/src/main/java/com/banco/Main/useCases/exceptionHandler/Exceptions.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Date;
 
 @ControllerAdvice
@@ -16,11 +17,19 @@ public class Exceptions extends ResponseEntityExceptionHandler {
         ResponseError responseError = new ResponseError(date, HttpStatus.BAD_REQUEST, "Documento Inválido");
         return ResponseEntity.status(responseError.httpStatus).body(responseError);
     }
-
     @ExceptionHandler(ContaNaoEncontradaError.class)
     public ResponseEntity contaInvalida(ContaNaoEncontradaError e) {
         var date = new Date();
         ResponseError responseError = new ResponseError(date, HttpStatus.NOT_FOUND, "Numero da conta não encontrado");
         return ResponseEntity.status(responseError.httpStatus).body(responseError);
     }
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity CpfCnpjJaCadastrados(SQLIntegrityConstraintViolationException e) {
+        var date = new Date();
+        ResponseError responseError = new ResponseError(date,HttpStatus.BAD_REQUEST, "CPF ou CNPJ ja cadastrado");
+        return ResponseEntity.status(responseError.httpStatus).body(responseError);
+    }
+
+
+
 }
